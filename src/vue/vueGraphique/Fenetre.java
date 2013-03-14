dc5b66b09e973f78aa705fe0e40da47fba69357b
Magoa <you@example.com>
2013-03-14 20:33:14 +0000

    Ajout de la gestion d'ajout des commande dans la GUI

--- src/vue/vueGraphique/Fenetre.java
+++ src/vue/vueGraphique/Fenetre.java
@@ -9,13 +9,16 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.IOException;
 import java.lang.Thread.State;
 import java.lang.management.ManagementFactory;
 import java.lang.management.OperatingSystemMXBean;
 import java.util.Map;
+import java.util.logging.Level;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -25,6 +28,7 @@ import javax.swing.JPanel;
 import javax.swing.JSlider;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+
 
 import controlleur.PEEM1000;
 
@@ -47,11 +51,10 @@ public class Fenetre extends JFrame{
 			
 	private JMenuItem demarrer = new JMenuItem("Demarrer");
 	private JMenuItem stopper = new JMenuItem("Stopper");
-	
-	private JTextArea resultat = new JTextArea();
+	private JMenuItem ajouterFichierCommande = new JMenuItem("Ajouter un fichier de commande");
 	
 	private JPanel principal = new JPanel(null);
-	
+	private JTextField texteNouvelleCommande = new JTextField();
 	private PEEM1000 peem1000;
 	/**
 	 * Constructeur mettant en place la disposition des composant
@@ -60,6 +63,7 @@ public class Fenetre extends JFrame{
      {
     	 JPanel panneauVitesse = new JPanel(), panneauAjoutCommande = new JPanel();
     	 JSlider slideVitesse = new JSlider();
+    	 JButton boutonAjouterCommande = new JButton("Ajouter");
     	 
 		this.setTitle("AGL");
 		this.setSize(700, 650);
@@ -73,28 +77,34 @@ public class Fenetre extends JFrame{
 		panneauVitesse.add(slideVitesse);
 		panneauVitesse.setBorder(BorderFactory.createTitledBorder("Modifier vitesse"));
 		
+		boutonAjouterCommande.addMouseListener(new AjouterCommande());
+		this.texteNouvelleCommande.addKeyListener(new AjouterCommande());
+		panneauAjoutCommande.add(this.texteNouvelleCommande);
+		panneauAjoutCommande.add(boutonAjouterCommande);
+		panneauAjoutCommande.setLayout(new GridLayout(1, 2));
 		panneauAjoutCommande.setBorder(BorderFactory.createTitledBorder("Ajouter une commande"));
 		
-		//this.principal.setLayout(new BorderLayout());
+		this.principal.setLayout(new GridLayout(3, 2));
 		this.fichierMenu.add(demarrer);
 		this.fichierMenu.add(stopper);
+		this.fichierMenu.add(ajouterFichierCommande);
 		
 		this.demarrer.addMouseListener(new DemarrerPEEM1000());
 		this.stopper.addMouseListener(new StopperPEEM1000());
+		this.ajouterFichierCommande.addMouseListener(new AjouterFichierCommande());
 		
 		this.barMenu.add(fichierMenu);
 		this.barMenu.add(aide);
 
+		this.principal.add(panneauVitesse);
+		this.principal.add(panneauAjoutCommande);
+		
 		this.setJMenuBar(barMenu);
 		this.setContentPane(principal);
 		this.setVisible(true);
              
      }
      
-     public JTextArea getResultat()
-	{
-		return resultat;
-	}
 
 	
 	/**
@@ -150,6 +160,65 @@ public class Fenetre extends JFrame{
 			peem1000.stopper();
       	}
 	}
+	
+	class AjouterFichierCommande extends MouseAdapter
+	{
+		private String cheminPrecedant;
+		public void mouseReleased(MouseEvent event)
+      	{
+			JFileChooser fileChooser = new JFileChooser(cheminPrecedant);
+			fileChooser.showOpenDialog(null);
+			if (fileChooser.getSelectedFile() != null)
+			{
+				try
+				{
+					cheminPrecedant = fileChooser.getSelectedFile().getCanonicalPath();
+					peem1000.ajouterCommandeFichier(cheminPrecedant);
+					
+				}
+				catch (IOException e1)
+				{
+					popupErreur(e1.getMessage());
+				}
+			}
+      	}
+	}
+	
+	class AjouterCommande extends MouseAdapter implements KeyListener
+	{
+		public void ajouterCommande()
+		{
+			String commande = texteNouvelleCommande.getText();
+			if(peem1000.ajouterCommande(commande) == true)
+			{
+				texteNouvelleCommande.setText("");
+			}
+			else
+			{
+				popupErreur("Commande non conforme");
+			}
+		}
+		public void mouseReleased(MouseEvent event)
+      	{
+			ajouterCommande();			
+      	}
+
+		
+		@Override
+		public void keyReleased(KeyEvent arg0) {
+			if(arg0.getKeyChar() == '\n')
+			{
+				ajouterCommande();
+			}
+		}
+
+		@Override
+		public void keyPressed(KeyEvent arg0){}
+
+		@Override
+		public void keyTyped(KeyEvent arg0) {}
+	}
+	
 	public static void main( String arg[] )
      {
     	new Fenetre();
